acd87fb36f56848943a11bfda3a282b190d47b2e
Wojciech Brüggemann <wojtek77@o2.pl>
2016-01-27 07:26:37 +0100

    my debug functions

--- public/index.php
+++ public/index.php
@@ -1,5 +1,8 @@
 <?php
 
+/* my debug functions */
+function v($v, $clearHtmlAndExit = false) { if ($clearHtmlAndExit) ob_end_clean(); echo '<pre>'; var_dump($v); echo '</pre>'; if ($clearHtmlAndExit) exit; } function vv($var, $level = 3, & $result = null, $isView = true) { if ($var && (is_array($var) || is_object($var))) { if ($level > 0) { if (is_object($var)) $var = vvvv($var); foreach ($var as $key => $value) vv($value, $level-1, $result[$key], false); } else { $result = '...any array or object'; } } else $result = $var; if ($isView) vvv($result); } function vvv($v, $clearHtmlAndExit = false) { if ($clearHtmlAndExit) ob_end_clean(); if (is_object($v)) $v = vvvv($v); echo '<pre>' . print_r($v, true) . '</pre>'; if ($clearHtmlAndExit) exit; } function vvvv($obj) { $arr = array(); $refl = new ReflectionClass($obj); foreach ($refl->getProperties() as $k => $prop) { $prop->setAccessible(true); $arr[$prop->getName()] = $prop->getValue($obj); } return $arr; } function d($limit = 0) { ob_start(); debug_print_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, $limit); $debug = ob_get_contents(); ob_end_clean(); vvv($debug); exit; }
+
 /**
  * Laravel - A PHP Framework For Web Artisans
  *
